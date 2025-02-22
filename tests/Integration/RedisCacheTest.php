875c6c4b894ccdcce485006a8f99ed3e38492aa7
Jakub Potocký <potockyjakub@gmail.com>
2025-02-22 18:36:27 +0100

    Tests wip

--- tests/Integration/RedisCacheTest.php
+++ tests/Integration/RedisCacheTest.php
@@ -7,6 +7,7 @@ namespace CoderaWorks\LaravelCacheCapsule\Tests\Integration;
 use Illuminate\Cache\CacheManager;
 use Illuminate\Contracts\Cache\Repository;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class RedisCacheTest extends TestCase
 {
@@ -19,12 +20,13 @@ class RedisCacheTest extends TestCase
         $this->cacheManager = $this->app->make('cache');
     }
 
-    public function testCacheWorkflowWithoutTagging(): void
+    #[DataProvider('provideCacheableData')]
+    public function testCacheWorkflowWithoutTagging(mixed $value): void
     {
-        $this->repository()->put('key', 10);
+        $this->repository()->put('key', $value);
 
         $this->assertSame(
-            10,
+            $value,
             $this->repository()->get('key'),
         );
 
@@ -37,25 +39,75 @@ class RedisCacheTest extends TestCase
         $this->assertNull($this->repository()->get('key'));
     }
 
-    public function testCacheWorkflowWithTagging(): void
+    #[DataProvider('provideCacheableData')]
+    public function testCacheWorkflowWithTagging(mixed $value): void
     {
-        $this->repository(['tag1'])->put('key', 10);
+        $this->repository('tag1')->put('key', $value);
 
         $this->assertSame(
-            10,
-            $this->repository(['tag1'])->get('key'),
+            $value,
+            $this->repository('tag1')->get('key'),
         );
 
-        $this->assertTrue($this->repository(['tag1'])->has('key'));
+        $this->assertTrue($this->repository('tag1')->has('key'));
 
-        $this->repository(['tag1'])->forget('key');
+        $this->repository('tag1')->forget('key');
 
-        $this->assertFalse($this->repository(['tag1'])->has('key'));
+        $this->assertFalse($this->repository('tag1')->has('key'));
 
-        $this->assertNull($this->repository(['tag1'])->get('key'));
+        $this->assertNull($this->repository('tag1')->get('key'));
     }
 
-    private function repository(array $tags = []): Repository
+    #[DataProvider('provideCacheableData')]
+    public function testTagsDoNotMatterWhenRetrievingCachedValue(mixed $value): void
+    {
+        $this->repository('tag1', 'tag2')->put('key', $value);
+
+        $this->assertSame($value, $this->repository()->get('key'));
+        $this->assertSame($value, $this->repository('tag1')->get('key'));
+        $this->assertSame($value, $this->repository('tag2')->get('key'));
+        $this->assertSame($value, $this->repository('tag1', 'tag2')->get('key'));
+        $this->assertSame($value, $this->repository('tag2', 'tag1')->get('key'));
+    }
+
+    public function testClearingTaggedCacheClearsValue(): void
+    {
+        $assertEmpty = function () {
+            $this->assertNull($this->repository()->get('key'));
+            $this->assertNull($this->repository('tag1')->get('key'));
+            $this->assertNull($this->repository('tag2')->get('key'));
+            $this->assertNull($this->repository('tag1', 'tag2')->get('key'));
+            $this->assertNull($this->repository('tag2', 'tag1')->get('key'));
+        };
+
+        $put = fn () => $this->repository('tag1', 'tag2')->put('key', 'value');
+
+        $put();
+
+        $this->repository('tag1')->forget('key');
+
+        $assertEmpty();
+
+        $put();
+
+        $this->repository('tag2')->forget('key');
+
+        $assertEmpty();
+
+        $put();
+
+        $this->repository('tag1', 'tag2')->forget('key');
+
+        $assertEmpty();
+
+        $put();
+
+        $this->repository('tag2', 'tag1')->forget('key');
+
+        $assertEmpty();
+    }
+
+    private function repository(string ...$tags): Repository
     {
         $repository = $this->cacheManager->driver('redis');
 
@@ -64,5 +116,56 @@ class RedisCacheTest extends TestCase
         }
 
         return $repository;
+    }
+
+    public static function provideCacheableData(): array
+    {
+        return [
+            [
+                0,
+            ],
+            [
+                1
+            ],
+            [
+                0.0,
+            ],
+            [
+                1.0,
+            ],
+            [
+                true,
+            ],
+            [
+                false,
+            ],
+            [
+                'string'
+            ],
+            [
+                [],
+            ],
+            [
+                [0],
+            ],
+            [
+                [1],
+            ],
+            [
+                [0.0],
+            ],
+            [
+                [1.0],
+            ],
+            [
+                [true],
+            ],
+            [
+                [false],
+            ],
+            [
+                ['string'],
+            ],
+        ];
     }
 }
