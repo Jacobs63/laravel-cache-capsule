b4f0fd9f75e3cd20ea592819e2436ae1f04d0885
Jakub Potocký <potockyjakub@gmail.com>
2025-02-22 18:11:05 +0100

    Refactoring

---
+++ tests/Integration/RedisCacheTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CoderaWorks\LaravelCacheCapsule\Tests\Integration;
+
+use Orchestra\Testbench\TestCase;
+
+class RedisCacheTest extends TestCase
+{
+
+}
